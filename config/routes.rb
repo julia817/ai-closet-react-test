a91a45e3c3d3dad59736a7851ba6b43d0cf0cf75
julia817 <zhushejia0817@gmail.com>
2019-11-28 10:25:45 +0900

    create database

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/index/App'
+  resources :clothes
+
   root 'pages#index'
 
   get 'pages/self_choose'
   get 'pages/ai_choose'
   get 'pages/outfits'
   get 'pages/store'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
