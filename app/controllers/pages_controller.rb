cedffe5d30b49836976f38770cb93ad3a15d2464
Darien <darien.miles@gmail.com>
2019-12-05 10:00:25 +0900

    react-rails/ routes and page controller updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
   def index
+    #changed variable names to camelCase for readability in javascript
     current = Weather.current
-    @currentWeather = current["weather"][0]["description"]
-    @current_degree = current["main"]["temp"]
+     @currentWeather = current["weather"][0]["description"]
+    @currentDegree = current["main"]["temp"]
     # puts @current_weather
     @forecast = Weather.forecast
     require 'json'
-   
+  
   end
 
   def self_choose
